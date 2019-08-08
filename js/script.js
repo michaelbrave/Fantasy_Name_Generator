27628dd9278bdf12c8cb1410a223b99e2b894e68
Michael Brave <mbrave@e1z3r4p3.42.us.org>
2019-08-08 14:47:02 -0700

    notn quite functional yet

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 
+function isVowel(c) {
+    return ['a', 'e', 'i', 'o', 'u'].indexOf(c.toLowerCase()) !== -1
+}
+
 function generate() {
 	var firstvar = document.getElementById('word1').value;
 	var lastvar = document.getElementById('word2').value;
@@ -7,22 +11,40 @@ function generate() {
 
 	for (var i = 0; i < firstvar.length; i++) {
 		letterarray.push(firstvar.slice(i, i + 2));
-	  }
+	}
 	for (var i = 0; i < lastvar.length; i++) {
 		letterarray.push(lastvar.slice(i, i + 2));
-	  }
+	}
 
-	for (var i = 0; i < 5; i++) {
+	for (var i = 0; i < 1000; i++) {
 		var word = "";
-		var rand_num = Math.floor(Math.random() * Math.floor(9));
+		var rand_num = Math.floor(Math.random() * Math.floor(5));
 		for (var j = 0; j < rand_num; j++) {
 			var rand_letter = Math.floor(Math.random() * letterarray.length);
 			word = word.concat(letterarray[rand_letter]);
 		}
-		word_combine.push(word);
-	  }
-	
-	for (var i = 0; i < word_combine.length; i++) {
-	document.getElementById('result').innerHTML = "<div><h2>" + word_combine + " " + "</h2></div>";
+		console.log("update " + word);
+		//struggling to get it to work where it throws away the junk but keeps the keepers
+		for (var b = 1; b < word.length; b++) {
+			if (word[b].isVowel == true && word[b - 1].isVowel == false) {
+				word_combine.push(word);
+				console.log("does it work " + word[b]);
+			}
+		}
+	}
+
+	//for (var i = 0; i < word_combine.length; i++) {
+	//document.getElementById('result').innerHTML = "<div><h2>" + word_combine + " " + "</h2></div>";
+	//} 
+
+	//how do I make it centered? containin another div? where how?
+	for (var c in word_combine) {
+		var newElement = document.createElement('result');
+		newElement.id = word_combine[c];
+		newElement.className = "newword";
+		newElement.innerHTML = "<div><h2>" + word_combine[c] + "</h2></div>";
+		document.body.appendChild(newElement);
 	}
 }
+
+//related maybe https://www.freecodecamp.org/forum/t/fun-with-pig-latin-challenge/246411/34

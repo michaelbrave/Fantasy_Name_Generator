1f356af981535597d773f79b08921dee1b038717
Michael Brave <mbrave@e1z1r6p20.42.us.org>
2019-08-07 22:02:44 -0700

    semi functional

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-function generate() {
-	var firstname = ["Marquis", "Samir", "Adrien", "Joyce", "Pierce", "Juliette", "Kelton", "Jacob", "Isiah", "Lindsay", "Kian", "Jordyn", "Jaquan", "Anya", "Wayne", "Khalil"];
-	var lastname = ["Mills", "Mercer", "Reeves", "Hines", "Sanford", "Irwin", "Koch", "Hinton", "Estes", "Jackson", "Lowe", "Guerra", "Pineda", "Franco", "Cowan", "Krause"];
-	var rand_first = Math.floor(Math.random() * firstname.length);
-	var rand_last = Math.floor(Math.random() * lastname.length);
-	document.getElementById('result').innerHTML = "<h1>My name is:</h1><div><h2>" + firstname[rand_first] + " " + lastname[rand_last] + "</h2></div>";
 
+function generate() {
+	var firstvar = document.getElementById('word1').value;
+	var lastvar = document.getElementById('word2').value;
+	var letterarray = [];
+	var word_combine = [];
+
+	for (var i = 0; i < firstvar.length; i++) {
+		letterarray.push(firstvar.slice(i, i + 2));
+	  }
+	for (var i = 0; i < lastvar.length; i++) {
+		letterarray.push(lastvar.slice(i, i + 2));
+	  }
+
+	for (var i = 0; i < 5; i++) {
+		var word = "";
+		var rand_num = Math.floor(Math.random() * Math.floor(9));
+		for (var j = 0; j < rand_num; j++) {
+			var rand_letter = Math.floor(Math.random() * letterarray.length);
+			word = word.concat(letterarray[rand_letter]);
+		}
+		word_combine.push(word);
+	  }
+	
+	for (var i = 0; i < word_combine.length; i++) {
+	document.getElementById('result').innerHTML = "<div><h2>" + word_combine + " " + "</h2></div>";
+	}
 }
